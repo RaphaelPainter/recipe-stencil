671fe2ac778b96278c44366d696e9ed7293c6e1d
RaphaelPainter <raphael.painter@imt-atlantique.net>
2023-12-15 11:39:13 +0100

    update to version 4.0.4

--- publish.sh
+++ publish.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-version="4.0.3"
+version="4.0.4"
 npm i
 npm version $version
 git add .
 git commit -m "update to version $version"
 git push
+sleep 10
 npm run build
 npm publish
 
